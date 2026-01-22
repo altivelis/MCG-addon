1ba506eb2eefa5c602fd013f2730b1274d16cdb2
altivelis <ayupon89821026@gmail.com>
2026-01-22 20:18:03 +0900

    大将死亡時に旧メッセージが出ていた問題の修正

--- behavior/scripts/die.js
+++ behavior/scripts/die.js
@@ -5,17 +5,17 @@ mc.world.afterEvents.entityDie.subscribe(data=>{
   const {deadEntity, damageSource} = data;
   if(deadEntity.typeId == "minecraft:player") return;
   if(deadEntity.typeId == "minecraft:item") return;
-  if(deadEntity.hasTag("ace")){
-    let aces = mc.world.getDimension("minecraft:overworld").getEntities({excludeTypes:["minecraft:player"],tags:[(deadEntity.hasTag("red")?"red":"blue"), "ace"]});
-    if(aces.length == 0){
-      let owners = mc.world.getPlayers({tags:[(deadEntity.hasTag("red")?"red":"blue")]});
-      owners.forEach(owner=>{
-        owner.removeTag("raid");
-        changeHealthBoost(owner, -2);
-        sendPlayerMessage(owner, "襲撃モードが終了しました");
-      })
-    }
-  }
+  // if(deadEntity.hasTag("ace")){
+  //   let aces = mc.world.getDimension("minecraft:overworld").getEntities({excludeTypes:["minecraft:player"],tags:[(deadEntity.hasTag("red")?"red":"blue"), "ace"]});
+  //   if(aces.length == 0){
+  //     let owners = mc.world.getPlayers({tags:[(deadEntity.hasTag("red")?"red":"blue")]});
+  //     owners.forEach(owner=>{
+  //       owner.removeTag("raid");
+  //       changeHealthBoost(owner, -2);
+  //       sendPlayerMessage(owner, "襲撃モードが終了しました");
+  //     })
+  //   }
+  // }
   if(damageSource.cause == mc.EntityDamageCause.selfDestruct) return;
   if((deadEntity.hasTag("call_pigman") || (deadEntity.typeId == "minecraft:pig" && getObject(deadEntity.hasTag("red")?"red":"blue").typeId == "minecraft:crying_obsidian")) && 
   (mc.world.getPlayers({tags:["turn"]})[0].hasTag("red")?deadEntity.hasTag("blue"):deadEntity.hasTag("red"))){
